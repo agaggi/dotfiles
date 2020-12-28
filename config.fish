7bebbb14e6f57bb7eb63ed0098925da63e60aa71
agaggi <gaggiotti.alex@gmail.com>
2020-12-28 16:01:34 -0500

    update

--- config.fish
+++ config.fish
@@ -14,8 +14,9 @@ source $OMF_PATH/init.fish
 alias cp="cp -i"                                            
 alias mv="mv -i"
 alias rm="rm -i"
-alias ls='ls --color'
+alias ls="ls --color=auto"
 
 alias df="df -h"                                    # Human-readable sizes
 alias free="free -m"                                # Show sizes in MB
 alias gitu="git add . && git commit && git push"	# Git commands
+
